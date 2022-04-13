7beb3ed7cae75b23c589c1281ace9f25f980c573
felixj0rdan <felixjordan312@gmail.com>
2022-04-13 11:22:14 +0530

    new

---
+++ StringMutable.java
@@ -0,0 +1,13 @@
+
+public class StringMutable {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sbf = new StringBuffer("felix");
+		sbf.append("j");
+		System.out.println(sbf);
+		
+
+	}
+
+}
---
+++ StringPool.java
@@ -0,0 +1,18 @@
+
+public class StringPool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "cat";
+		String s2 = "cat";
+		
+		System.out.println(s1==s2);
+		
+		String a = "cat";
+		String b = new String(a);
+		
+		System.out.println(a==b);
+
+	}
+
+}
